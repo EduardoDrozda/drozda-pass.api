19b84fe646fd5c4bf645a5bb9a20f1029f17e47c
Eduardo Reis <edu_drozda@hotmail.com>
2023-11-23 23:35:07 -0300

    Config enviroment variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app
   .listen({
     host: "0.0.0.0",
-    port: 33333,
+    port: env.PORT,
   })
   .then((address) => {
     console.log(`Server listening at ${address}`);
